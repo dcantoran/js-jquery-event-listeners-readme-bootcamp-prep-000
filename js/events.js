38287f0f27b9855032733121a1ceea801fc67af2
David Cantoran <davidcantoran36@gmail.com>
2019-03-07 18:41:43 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $(document).on('keydown', function(key) {
+  $('input').on('keydown', function(key) {
     if(key.which === '71') {
       window.alert("G was pressed")
     }
